bfedda9f78c5c85b0cc09ef61fb06e517e42753b
Myschoolmanager <myschoolmanager11@gmail.com>
2026-03-08 18:44:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ const FILE_ITEMS = {
 //"قائمة التلاميذ الغائبون قبل اليوم": CONFIG.Old_Absented_File_ID,
 //"متابعة غيابات اليوم": CONFIG.New_Absented_File_ID,
 
-const GAS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw72dIKiuWMSm_kFb1E0hruYZmKVyO40Zft1d8IfcugclUXqGAb2hWi5q2MhcVNIIFi/exec";
+const GAS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwramM2wiY9TlOyUNrfMD6X6J8bEzE0tIMwqFCHfCZ_AmvZwlLzPLzeafbqh9Xky1Te/exec";
 
 let currentFileURL = null;
 let PASSWORDS = [];
@@ -1383,6 +1383,7 @@ function DownloadNewAbsented() {
 
     window.open(downloadUrl, "_blank");
 }
+
 
 
 
