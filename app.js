15426f280d816d98974d752f4a46e41af9f880b0
Myschoolmanager <myschoolmanager11@gmail.com>
2026-03-18 13:22:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -428,7 +428,7 @@ loginBtn.addEventListener("click", function(){
     const type = userTypeSelect.value;
     let racordInputValue = racordInput.value; // القيمة الخام من الحقل
 
-   if(!type || type === "-- اختر --"){
+   if(!type){
     showToast("يرجى اختيار نوع المستخدم", "warning");
     return;
 }
