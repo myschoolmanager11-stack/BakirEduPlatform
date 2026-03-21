78b34aea52cd4f63fa2b27a0bf35b19c3e6ae582
Myschoolmanager <myschoolmanager11@gmail.com>
2026-03-21 19:56:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -509,33 +509,7 @@ loginBtn.addEventListener("click", function(){
 function openSession(type, user) {
   
     showToast("تم تسجيل الدخول بنجاح", "success"); 
-
-// 👇 إرسال الإعدادات مرة واحدة فقط
-if(!localStorage.getItem("configSent_" + CONFIG.School_Folder_ID)){
-
-  const data = new URLSearchParams();
-
-  data.append("action", "saveConfig");
-  data.append("School_Folder_ID", CONFIG.School_Folder_ID);
-  data.append("SchoolName", CONFIG.SchoolName);
-  data.append("New_Absented_File_ID", CONFIG.New_Absented_File_ID);
-  data.append("Daily_absences_Folder_ID", CONFIG.Daily_absences_Folder_ID);
-
-  fetch(GAS_SCRIPT_URL, { // ✅ بدون ""
-    method: "POST",
-    body: data
-  })
-  .then(res => res.text())
-  .then(res => {
-    console.log("CONFIG RESPONSE:", res);
-
-    if(res === "CONFIG_SAVED"){
-      localStorage.setItem("configSent_" + CONFIG.School_Folder_ID, "true");
-    }
-  });
-}
-
-  
+ 
     console.log("فتح الجلسة للمستخدم:", type);
 
     document.body.style.pointerEvents = "auto";
