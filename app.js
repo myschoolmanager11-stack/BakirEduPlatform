dbc215b07b72ee2cdd830a191af9fc293861f0f2
Myschoolmanager <myschoolmanager11@gmail.com>
2026-03-21 17:58:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ const FILE_ITEMS = {
 
 // ==================== Google Apps Script رابط ====================
 
-const GAS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzme0WghyDcaff438w3fGYEZvEyV_f-GaShCalHaQ4tjQOcmGL2jXKNwdOZrcr8iMB6/exec";
+const GAS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzYLN0d-JKWPk6tK3G738QMl3sphHrNJXXAVBgR9ZSJTYAyUyDFZhquBL5DN_t4yZnK/exec";
 
 // ==================== متغيرات عامة ====================
 
